6c6cec7e40ae1bdc27cb329ab0ee7b58fee5409f
Xiaojin Li <shorjin09@gmail.com>
2023-09-29 12:07:37 -0400

    try again

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,13 @@
-# calculator functions
+"""
+Calculator functions
+"""
+
 
 def add(x, y):
-    return x+y
+    """Add x and y and return result."""
+    return x + y
 
 
 def subtract(x, y):
-    return y-x
+    """Subtract x from y and return result."""
+    return y - x
